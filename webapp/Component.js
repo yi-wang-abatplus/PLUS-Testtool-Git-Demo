92605a0a39309bae0d052bbeadaa55b17e73525f
yi-wang-abatplus <yi.wang@abatplus.de>
2017-09-25 13:49:41 +0000

    Original Coding
    Change-Id: I1f5fa3143ce6ad051d6ba2fb507cac5154cb94ad

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("zywa.testzywa_order_info_app_local_300.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("zywa.testzywa_order_info_app_local_300.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
